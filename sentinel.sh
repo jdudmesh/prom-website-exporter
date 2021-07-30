2ebf2d91178b427e69bfd2b0fd393c907002cfb0
John Dudmesh <john@johndudmesh.com>
2021-07-30 13:22:49 +0200

    amended sentinel

--- sentinel.sh
+++ sentinel.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-if [ ! -f /var/run/monitor.pid ]; then
+if [ ! -f /var/run/prom-website-exporter.pid ]; then
     echo "Monitor not running"
     /home/ec2-user/prom-website-exporter/monitoring/run.sh
 fi
