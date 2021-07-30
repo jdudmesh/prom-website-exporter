28a78b7e2866002a4a5c1fde82329157fed36e8b
John Dudmesh <john@johndudmesh.com>
2021-07-30 13:15:23 +0200

    Back to nohup

--- run.sh
+++ run.sh
@@ -1,10 +1,3 @@
 #! /bin/bash
 
-/usr/local/sbin/daemonize -a \
-    -c /home/ec2-user/prom-website-exporter \
-    -e /var/log/monitor.log \
-    -o /var/log/monitor.log \
-    -p /var/run/monitor.pid \
-    -l /var/run/monitor.pid \
-    /home/ec2-user/prom-website-exporter/monitoring
-
+/bin/nohup /home/ec2-user/prom-website-exporter &
